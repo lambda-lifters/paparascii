965685487031c848ca169d19b0bfea278b1d9d12
Tim Brown <tim@timb.net>
2025-10-02 14:10:30 +0000

    added build-and-deploy workflow for clients

--- src/lambda_lifters/ascii_blog/init.clj
+++ src/lambda_lifters/ascii_blog/init.clj
@@ -9,31 +9,35 @@
   "Initialize a new site with basic structure"
   [& {:keys [name] :or {name "My Blog"}}]
   (log/in-section
-    (str "Initializing new site: " name)
-    (let [config {:site-name      name
-                  :copyright-date "2024"
-                  :contact-email  "email@example.com"
-                  :site-lead      "Welcome to my blog"
-                  :site-about     "A blog about interesting things"
-                  :links          [{:link-name "GitHub" :link-url "https://github.com"}
-                                   {:link-name "Twitter" :link-url "https://twitter.com"}]}]
-      (log/in-section
-        "Create directories"
-        (doseq [dir ["blog" "site" "resources" "assets/css" "assets/js" "assets/img" "templates"]]
-          (u/ensure-directory dir)))
-      (log/in-section
-        "Create site-config.edn"
-        (spit "site-config.edn" (with-out-str (pp/pprint config))))
-      (log/in-section
-        "Copy default templates from resources"
+   (str "Initializing new site: " name)
+   (let [config {:site-name name
+                 :copyright-date "2024"
+                 :contact-email "email@example.com"
+                 :site-lead "Welcome to my blog"
+                 :site-about "A blog about interesting things"
+                 :links [{:link-name "GitHub" :link-url "https://github.com"}
+                         {:link-name "Twitter" :link-url "https://twitter.com"}]}]
+     (log/in-section
+      "Create directories"
+      (doseq [dir ["blog" "site" "resources" "assets/css" "assets/js" "assets/img" "templates" ".github/workflows"]]
+        (u/ensure-directory dir)))
+     (log/in-section
+      "Create site-config.edn"
+      (spit "site-config.edn" (with-out-str (pp/pprint config))))
+     (log/in-section
+      "Copy default templates from resources"
 
-        (when-let [template-site (u/resource-path "template-site")]
-          (doseq [template-file (file-seq (.toFile template-site))
-                  :when (.isFile template-file)]
-            (let [relative-file (.toFile (Path/.relativize template-site (.toPath template-file)))
-                  destination-file (site/site-path relative-file)]
-              (u/copy-file template-file destination-file)))))
-      (log/success "
+      (when-let [template-site (u/resource-path "template-site")]
+        (doseq [template-file (file-seq (.toFile template-site))
+                :when (.isFile template-file)]
+          (let [relative-file (.toFile (Path/.relativize template-site (.toPath template-file)))
+                destination-file (site/site-path relative-file)]
+            (u/copy-file template-file destination-file)))))
+     (log/in-section
+      "Copy GitHub workflow"
+      (when-let [workflow-template (u/resource-path "template-github-workflow-build-and-deploy.yml")]
+        (u/copy-file (.toFile workflow-template) (site/site-path ".github/workflows/build-and-deploy.yml"))))
+     (log/success "
       Site initialized! Next steps:
         1. Edit site-config.edn to customize your site
         2. Run: clojure -Tascii-blog build
